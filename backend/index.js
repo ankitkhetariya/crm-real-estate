d69fdd259388d9746fc3ca4153e3c342e888474d
mohit khetariya <153801182+ankitkhetariya@users.noreply.github.com>
2026-01-28 10:12:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 // ✅ Middleware
 app.use(cors({
-    origin: "http://localhost:5173", // Frontend URL
+    origin: ["http://localhost:5173", "https://crm-mohitrealestate.netlify.app"], 
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"]
 }));
